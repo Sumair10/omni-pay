247e2b2be5003e6da2da7c6039bb4fb5802771d4
Sumair10 <sumairzahid123@gmail.com>
2023-01-30 18:46:52 +0500

    landing page

---
+++ src/sections/faqs/FaqsList.js
@@ -0,0 +1,31 @@
+// @mui
+import {Typography, Accordion,  AccordionSummary, AccordionDetails } from '@mui/material';
+// _mock_
+import { _faqs } from '../../_mock';
+// components
+import Iconify from '../../components/Iconify';
+
+// ----------------------------------------------------------------------
+
+export default function FaqsList() {
+  return (
+    <>
+      {_faqs.map((accordion) => (
+        <Accordion key={accordion.id}>
+          <AccordionSummary
+            expandIcon={<Iconify icon={'eva:arrow-ios-downward-fill'} width={20} height={20} />}
+          >
+            <Typography variant="subtitle1">{accordion.heading}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{accordion.detail}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </>
+  );
+}
+
+git add .
+git commit -m "landing page"
+git push
